1bba1b7e7da52ff7dc29e8c7eda9eea920faf606
BigOyayubi <55793780+BigOyayubi@users.noreply.github.com>
2019-11-10 23:03:36 +0900

    zstd decompress api追加

---
+++ source/zstd_wrap.c
@@ -0,0 +1,37 @@
+#include <lauxlib.h>
+#include <lua.h>
+#include "zstd.h"
+
+static int decompress(lua_State* L)
+{
+  size_t len = 0;
+  char*       dst     = (char*)      luaL_checklstring(L, 1, &len);
+  size_t      dstSize = (size_t)     luaL_checkinteger(L, 2      );
+  const char* src     = (const char*)luaL_checklstring(L, 3, &len);
+  size_t      srcSize = (size_t)     luaL_checkinteger(L, 4      );
+
+  size_t result = ZSTD_decompress(dst, dstSize, src, srcSize);
+  lua_pushinteger(L, result);
+  return 1;
+}
+
+static int isError(lua_State* L)
+{
+  size_t code = (size_t)luaL_checkinteger(L, 1);
+  unsigned result = ZSTD_isError(code);
+  lua_pushboolean(L, result);
+  return 1;
+}
+
+static const luaL_Reg zstd_functions[] = {
+  { "decompress", decompress },
+  { "isError",    isError    },
+  { NULL,         NULL       }
+};
+
+LUALIB_API int luaopen_zstd(lua_State* L)
+{
+  luaL_register(L, "zstd", zstd_functions);
+  return 1;
+}
+
