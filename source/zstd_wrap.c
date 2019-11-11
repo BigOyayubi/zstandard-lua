c838d7cd8496b5a318f0c50242816b022d7ca6bb
BigOyayubi <55793780+BigOyayubi@users.noreply.github.com>
2019-11-11 18:27:21 +0900

    add decompress with dictionary api

--- source/zstd_wrap.c
+++ source/zstd_wrap.c
@@ -1,7 +1,45 @@
+//#define  ENABLE_LOG
+
 #include <lauxlib.h>
 #include <lua.h>
 #include "zstd.h"
+#include "decompress/zstd_decompress_internal.h"
 
+#ifdef ENABLE_LOG
+  #include <stdio.h>
+  #define debug_log( fmt, ... ) \
+    fprintf( stderr, fmt, ##__VA_ARGS__ )
+#else
+  #define debug_log( fmt, ... ) ((void)0)
+#endif
+
+/***************************************************************
+ * Basic functions 
+ * *************************************************************/
+
+/* 
+ * check is error that zstd function return value
+ */
+static int isError(lua_State* L)
+{
+  size_t code = (size_t)luaL_checkinteger(L, 1);
+  unsigned result = ZSTD_isError(code);
+  lua_pushboolean(L, result);
+  return 1;
+}
+
+/*
+ * Decompress without dictionary
+ * lua sample
+ *   local zstd = require("zstd")
+ *   local dst = (" "):rep(256)
+ *   local f = io.open("path/to/src")
+ *   local src = f:read("*a")
+ *   f:close()
+ *
+ *   local result = zstd.decompress(dst, string.len(dst), src, string.len(src))
+ *   local isError = zstd.isError(result)
+ */
 static int decompress(lua_State* L)
 {
   size_t len = 0;
@@ -15,18 +53,141 @@ static int decompress(lua_State* L)
   return 1;
 }
 
-static int isError(lua_State* L)
+/***************************************************************
+ * Dictionary Decompress
+ * *************************************************************/
+
+typedef struct
 {
-  size_t code = (size_t)luaL_checkinteger(L, 1);
-  unsigned result = ZSTD_isError(code);
-  lua_pushboolean(L, result);
+  ZSTD_DDict* ddict;
+  ZSTD_DCtx*  dctx;
+} zstd_decompress_dict_t;
+#define ZSTD_DICTHANDLE "zstd.decomp_dictionary"
+
+static zstd_decompress_dict_t* _get_zstd_decompress_dict_t(lua_State *L, int index)
+{
+  return (zstd_decompress_dict_t*)luaL_checkudata(L, index, ZSTD_DICTHANDLE);
+}
+
+static int zstd_decompress_dict_tostring(lua_State *L)
+{
+  zstd_decompress_dict_t* context = _get_zstd_decompress_dict_t(L, 1);
+  lua_pushfstring(L, "%s (%p)", ZSTD_DICTHANDLE, context);
+  return 1;
+}
+
+static int zstd_decompress_dict_gc(lua_State *L)
+{
+  zstd_decompress_dict_t* context = _get_zstd_decompress_dict_t(L, 1);
+  ZSTD_freeDDict(context->ddict);
+  ZSTD_freeDCtx(context->dctx);
+  return 0;
+}
+
+static int decompressDict(lua_State *L)
+{
+  zstd_decompress_dict_t* context = _get_zstd_decompress_dict_t(L, 1);
+
+  size_t len = 0;
+  char*       dst      = (char*)      luaL_checklstring(L, 2, &len);
+  size_t      dstSize  = (size_t)     luaL_checkinteger(L, 3      );
+  const char* src      = (const char*)luaL_checklstring(L, 4, &len);
+  size_t      srcSize  = (size_t)     luaL_checkinteger(L, 5      );
+
+  debug_log("context->dctx    %p\n", context->dctx);
+  debug_log("context->ddict   %p\n", context->ddict);
+  debug_log("dst     %p\n", dst);
+  debug_log("dstSize %zu\n", dstSize);
+  debug_log("src     %p\n", src);
+  debug_log("srcSize %zu\n", srcSize);
+
+  size_t result = ZSTD_decompress_usingDDict(context->dctx, dst, dstSize, src, srcSize, context->ddict);
+  debug_log("result %zu\n", result);
+  if(result != dstSize)
+  {
+    lua_pushliteral(L, "call to ZSTD_decompress_usingDDict failed");
+    lua_error(L);
+  }
+
+  lua_pushinteger(L, result);
+  return 1;
+}
+
+static const luaL_Reg decompress_dict_functions[] = {
+  { "decompressDict", decompressDict },
+  { NULL,             NULL           }
+};
+
+/*
+ * load zstd dictionary
+ * lua sample
+ *   local zstd = require("zstd")
+ *   local f = io.open("path/to/dict")
+ *   local data = f:read("*a")
+ *   f:close()
+ *   local dataSize = string.len(data)
+ *   local dict = zstd.loadDictionary( data, dataSize )
+ *   f = io.open("path/to/src")
+ *   local src = f:read("*a")
+ *   f:close()
+ *   local srcSize = string.len(src)
+ *   local dst = (" "):rep(100)
+ *   local dstSize = string.len(dst)
+ *   local result = dict:decompressDict(dst, dstSize, src, srcSize)
+ *   local isError = zstd.isError( result )
+ */
+static int loadDictionary(lua_State* L)
+{
+  size_t len = 0;
+  const char* data = (const char*)luaL_checklstring(L, 1, &len);
+  size_t      dataSize = (size_t)luaL_checkinteger(L, 2);
+
+  ZSTD_DDict* const ddict = ZSTD_createDDict(data, dataSize);
+  if(ddict == NULL)
+  {
+    lua_pushliteral(L, "call to ZSTD_createDDict failed");
+    lua_error(L);
+  }
+
+  ZSTD_DCtx* const dctx = ZSTD_createDCtx();
+  if(dctx == NULL)
+  {
+    lua_pushliteral(L, "call to ZSTD_createDCtx failed");
+    lua_error(L);
+  }
+
+  debug_log("dctx    %p\n", dctx);
+  debug_log("ddict   %p\n", ddict);
+ 
+  zstd_decompress_dict_t* context = (zstd_decompress_dict_t*)lua_newuserdata(L, sizeof(zstd_decompress_dict_t));
+  context->ddict = ddict;
+  context->dctx  = dctx;
+  if(luaL_newmetatable(L, ZSTD_DICTHANDLE))
+  {
+    // new method table
+    luaL_newlib(L, decompress_dict_functions);
+
+    // metatable.__index = method table
+    lua_setfield(L, -2, "__index");
+
+    // metatable.__tostring
+    lua_pushcfunction(L, zstd_decompress_dict_tostring);
+    lua_setfield(L, -2, "__tostring");
+    
+    // metatable.__gc
+    lua_pushcfunction(L, zstd_decompress_dict_gc);
+    lua_setfield(L, -2, "__gc");
+  }
+  lua_setmetatable(L, -2);
+
   return 1;
 }
 
 static const luaL_Reg zstd_functions[] = {
-  { "decompress", decompress },
-  { "isError",    isError    },
-  { NULL,         NULL       }
+  { "decompress",     decompress     },
+  { "isError",        isError        },
+  { "loadDictionary", loadDictionary },
+  { NULL,             NULL           }
 };
 
 LUALIB_API int luaopen_zstd(lua_State* L)
--- test.lua
+++ test.lua
@@ -18,9 +18,22 @@ local read_testfile = function( decompressed_filename )
     return decompressed, decompressed_size, compressed, compressed_size
 end
 
+local read_testfile2 = function( decompressed_file_prefix )
+    local decompressed = readfile(decompressed_file_prefix .. ".txt" )
+    local decompressed_size = string.len(decompressed)
+
+    local compressed = readfile(decompressed_file_prefix .. ".zstd" )
+    local compressed_size = string.len(compressed)
+
+    local dictionary = readfile(decompressed_file_prefix .. ".dict")
+    local dictionary_size = string.len(dictionary)
+
+    return decompressed, decompressed_size, compressed, compressed_size, dictionary, dictionary_size
+end
+
 describe("decompress method test", function()
   it("return value check", function()
-    local decompressed, decompressed_size, compressed, compressed_size = read_testfile( 'testdata/test1.txt' )
+    local decompressed, decompressed_size, compressed, compressed_size = read_testfile( 'testdata/test1/test1.txt' )
     local dst = (" "):rep(decompressed_size)
     local result = zstd.decompress( dst, decompressed_size, compressed, compressed_size )
     
@@ -28,7 +41,7 @@ describe("decompress method test", function()
   end)
 
   it("decompressed data check", function()
-    local decompressed, decompressed_size, compressed, compressed_size = read_testfile( 'testdata/test1.txt' )
+    local decompressed, decompressed_size, compressed, compressed_size = read_testfile( 'testdata/test1/test1.txt' )
     local dst = (" "):rep(decompressed_size)
     local result = zstd.decompress( dst, decompressed_size, compressed, compressed_size )
 
@@ -36,7 +49,7 @@ describe("decompress method test", function()
   end)
 
   it("return value isError check", function()
-    local decompressed, decompressed_size, compressed, compressed_size = read_testfile( 'testdata/test1.txt' )
+    local decompressed, decompressed_size, compressed, compressed_size = read_testfile( 'testdata/test1/test1.txt' )
     local dst = (" "):rep(decompressed_size)
     local result = zstd.decompress( dst, decompressed_size, compressed, compressed_size )
     local isError = zstd.isError( result )
@@ -54,4 +67,17 @@ describe("decompress method test", function()
   end)
 end)
 
+describe("decompress method with dictionary test", function()
+  it("load dictionary test", function()
+    local dst, dst_size, src, src_size, dict_data, dict_data_size = read_testfile2( 'testdata/test2/test2' )
+    local dict = zstd.loadDictionary( dict_data, dict_data_size )
+  end)
 
+  it("decompress with dictionary test", function()
+    local dst, dst_size, src, src_size, dict_data, dict_data_size = read_testfile2( 'testdata/test2/test2' )
+    local dict = zstd.loadDictionary( dict_data, dict_data_size )
+    local result = dict:decompressDict( dst, dst_size, src, src_size )
+    local isError = zstd.isError( result )
+    assert.are.equal( isError, false )
+  end)
+end)
